a88190cf11e06fbbca2fefb77ceea9cbd8a45bf8
superman566 <guochaohetony@gmail.com>
2021-12-11 13:47:46 -0500

    fix: format

--- release.config.js
+++ release.config.js
@@ -4,11 +4,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      asserts: [
-        { path: "build.zip", label: "Build" },
-        { path: "coverage.zip", label: "coverage" }
-      ]
-    }]
+    [
+      "@semantic-release/github",
+      {
+        asserts: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "coverage" },
+        ],
+      },
+    ],
   ],
 };
